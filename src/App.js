7bdbb384f7912743c94715268c2d091d51e3e96f
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-02-17 22:34:54 +0600

    dynamically add data for banner with gradient overlay

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import router from "./Router/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
+    <div data-theme="night" className="max-w-screen-xl mx-auto">
       <RouterProvider router={router}>
 
       </RouterProvider>
